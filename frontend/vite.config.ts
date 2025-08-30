772bd563e10d0ea9c53fb421a216e2e6c206296f
IsraelHub <israelcunhaolive@gmail.com>
2025-08-30 11:47:46 -0300

    build: configura toolchain do frontend (Vite + TS)

---
+++ frontend/vite.config.ts
@@ -0,0 +1,17 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig(({ mode }) => {
+    return {
+      envDir: '..',
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, '.'),
+        }
+      },
+      server: {
+        port: 5173,
+        host: true
+      }
+    };
+});
