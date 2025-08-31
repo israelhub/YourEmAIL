d54de69b43d954bf9bd0945089bbffcb7e6f9446
IsraelHub <israelcunhaolive@gmail.com>
2025-08-31 11:14:15 -0300

    feat: barrel de services

---
+++ frontend/src/services/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Índice dos serviços - facilita imports
+ */
+
+export { httpService } from './httpService';
+export { authService } from './authService';
+export { gmailService } from './gmailService';
+export { analyzeEmailContent } from './geminiService';
